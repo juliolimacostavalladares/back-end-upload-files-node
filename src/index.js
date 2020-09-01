64b019a11bef3203a598f89bbb375eee36b4f725
Julio Lima Costa Valladres <juliolima1225@gmail.com>
2020-09-01 12:37:13 -0300

    add port from heroku

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-require('dotenv').config()
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const cros = require('cors');
+const cros = require("cors");
 
 const app = express();
 
 app.use(cros());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(morgan('dev'));
-app.use(require('./routes/routes'));
+app.use(morgan("dev"));
+app.use(require("./routes/routes"));
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
